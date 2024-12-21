8cac1067c3877b01fba9e8511e0ccfc421fcc28f
abujaforhadi <abujaforhadi@gmail.com>
2024-12-21 23:53:46 +0600

    setup

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 3000;  
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.user}:${process.env.pass}@cluster0.006kz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+ function run() {
+  try {
+     client.connect();
+    
+
+     client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+  }
+}
+
+run();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Server!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
