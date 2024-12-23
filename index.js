b7241327345d9a1255073757ed83e48c8df5b23d
abujaforhadi <abujaforhadi@gmail.com>
2024-12-23 14:28:07 +0600

    all tutors

--- index.js
+++ index.js
@@ -25,8 +25,42 @@ const client = new MongoClient(uri, {
      client.connect();
     
 
-     client.db("admin").command({ ping: 1 });
+    //  client.db("admin").command({ ping: 1 });
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const db = client.db("TutorZon");
+    const collection = db.collection("languages");
+    app.post("/find-tutors", async (req, res) => {
+      try {
+        const data = await collection.insertOne(req.body);
+        res.json(data);
+      } catch (error) {
+        // console.error("Error inserting data into database:", error);
+        res.status(500).send("Error inserting data into database");
+      }
+    });
+    app.get("/find-tutors", async (req, res) => {
+      try {
+        const data = await collection.find({}).toArray();
+        res.json(data);
+      } catch (error) {
+        res.status(500).send("Error fetching data from database");
+      }
+    });
+    app.get("(/tutor/:details", async (req, res) => {
+      const { id } = req.params;
+      try {
+        const product = await collection.findOne({ _id: new ObjectId(id) });
+        if (product) {
+          res.json(product);
+        } else {
+          res.status(404).json({ error: "Product not found" });
+        }
+      } catch (error) {
+        // console.error("Error fetching product:", error);
+        res.status(500).json({ error: "Error fetching product" });
+      }
+    });
 
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
